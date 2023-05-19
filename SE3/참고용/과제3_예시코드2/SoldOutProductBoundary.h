28a1dd9575386af81af2acd8e9597af02d3deba1
kkongchi <smallbean99s@gmail.com>
2023-05-20 03:09:46 +0900

    directory edit

--- SE3/참고용/과제3_예시코드2/SoldOutProductBoundary.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <iostream>
-
-#include "entity.h"
-
-class SoldOutProductBoundary {
-	public:
-		void printSoldOutProduct(vector<Product*> soldOutList);
-};
--- SE3/참고용/과제3_예시코드2/my.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include "entity.h"
-#include "control.h"
-#include "boundary.h"
